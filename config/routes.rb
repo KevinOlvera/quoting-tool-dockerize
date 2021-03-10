410792df5366930cf2461367445f4e1faf3fb672
Kevin Olvera <kevin.olvera@nokia.com>
2021-03-10 12:59:18 -0600

    added devise styles

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :nuage_versions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "quotes#index"
 end
